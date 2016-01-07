086a2bb0be83e1476bb9e10ee1d4f5cbf92cef36
Jaak Simm <jaak.simm@gmail.com>
2016-01-07 02:54:01 +0200

    Ytest for bpmf

--- python/macau/macau.pyx
+++ python/macau/macau.pyx
@@ -16,6 +16,11 @@ def bpmf(Y,
          nsamples   = 400):
     if type(Y) != sp.sparse.coo.coo_matrix:
         raise ValueError("Y must be scipy.sparse.coo.coo_matrix")
+    if Ytest != None:
+        if type(Ytest) != sp.sparse.coo.coo_matrix:
+            raise ValueError("Ytest must be scipy.sparse.coo.coo_matrix")
+        if Ytest.shape != Y.shape:
+            raise ValueError("Ytest and Y must have the same shape")
 
     cdef np.ndarray[int] irows = Y.row.astype(np.int32, copy=False)
     cdef np.ndarray[int] icols = Y.col.astype(np.int32, copy=False)
@@ -26,6 +31,16 @@ def bpmf(Y,
     macau.setPrecision(np.float64(precision))
     macau.setRelationData(&irows[0], &icols[0], &ivals[0], irows.shape[0], Y.shape[0], Y.shape[1]);
     macau.setSamples(np.int32(burnin), np.int32(nsamples))
+
+    cdef np.ndarray[int] trows, tcols
+    cdef np.ndarray[np.double_t] tvals
+
+    if Ytest != None:
+        trows = Ytest.row.astype(np.int32, copy=False)
+        tcols = Ytest.col.astype(np.int32, copy=False)
+        tvals = Ytest.data.astype(np.double, copy=False)
+        macau.setRelationDataTest(&trows[0], &tcols[0], &tvals[0], trows.shape[0], Y.shape[0], Y.shape[1])
+
     macau.run()
     return dict(rmse_test = macau.getRmseTest())                                                                                                         
 
--- python/macau/test/test_bpmf.py
+++ python/macau/test/test_bpmf.py
@@ -8,6 +8,10 @@ class TestBPMF(unittest.TestCase):
     def test_bpmf(self):
         X = scipy.sparse.rand(15, 10, 0.2)
         macau.bpmf(X, num_latent = 10)
+    def test_bpmf2(self):
+        X = scipy.sparse.rand(15, 10, 0.2)
+        Xt = scipy.sparse.rand(15, 10, 0.1)
+        macau.bpmf(X, Xt, num_latent = 10)
 
 if __name__ == '__main__':
     unittest.main()
