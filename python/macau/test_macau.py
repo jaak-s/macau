c4c5784fbcc9a8902ffe4b44ca5910f1c7772ac1
Jaak Simm <jaak.simm@gmail.com>
2016-10-21 17:02:09 +0200

    test for univariate Macau

--- python/macau/test_macau.py
+++ python/macau/test_macau.py
@@ -7,7 +7,9 @@ class TestMacau(unittest.TestCase):
     def test_bpmf(self):
         Y = scipy.sparse.rand(10, 20, 0.2)
         Y, Ytest = macau.make_train_test(Y, 0.5)
-        results = macau.bpmf(Y, Ytest = Ytest, num_latent = 4, verbose = False, burnin = 50, nsamples = 50)
+        results = macau.bpmf(Y, Ytest = Ytest, num_latent = 4,
+                             verbose = False, burnin = 50, nsamples = 50,
+                             univariate = False)
         self.assertEqual(Ytest.nnz, results.prediction.shape[0])
 
     def test_macau(self):
@@ -17,7 +19,19 @@ class TestMacau(unittest.TestCase):
         side2   = scipy.sparse.coo_matrix( np.random.rand(20, 3) )
 
         results = macau.bpmf(Y, Ytest = Ytest, side = [side1, side2], num_latent = 4,
-                             verbose = False, burnin = 50, nsamples = 50)
+                             verbose = False, burnin = 50, nsamples = 50,
+                             univariate = False)
+        self.assertEqual(Ytest.nnz, results.prediction.shape[0])
+
+    def test_macau_univariate(self):
+        Y = scipy.sparse.rand(10, 20, 0.2)
+        Y, Ytest = macau.make_train_test(Y, 0.5)
+        side1   = scipy.sparse.coo_matrix( np.random.rand(10, 2) )
+        side2   = scipy.sparse.coo_matrix( np.random.rand(20, 3) )
+
+        results = macau.bpmf(Y, Ytest = Ytest, side = [side1, side2], num_latent = 4,
+                             verbose = False, burnin = 50, nsamples = 50,
+                             univariate = True)
         self.assertEqual(Ytest.nnz, results.prediction.shape[0])
 
 if __name__ == '__main__':
